bbab0c042a226c72d1c6aa80605e857285122c79
Bram Adl <thenamesbram@gmail.com>
2022-06-23 01:27:01 +0700

    feat: list of table

---
+++ src/components/Tables/ListOfTable.vue
@@ -0,0 +1,212 @@
+<script setup>
+import { computed, onMounted, ref } from 'vue'
+
+import { useMainStore } from '@/stores/main'
+import ModalBox from '@/components/ModalBox.vue'
+import CheckboxCell from '@/components/CheckboxCell.vue'
+import Level from '@/components/Level.vue'
+import JbButtons from '@/components/JbButtons.vue'
+import JbButton from '@/components/JbButton.vue'
+import { useFetch } from '@/composition/useFetch'
+import { useEmitter } from '@/composition/useEmitter'
+
+const emitter = useEmitter()
+
+emitter.on('refresh:tables', async () => {
+  const { data: { data: tables } } = await useFetch('get', props.endpoint)
+  items.value = tables
+})
+
+const props = defineProps({
+  checkable: Boolean,
+  crud: {
+    type: Boolean,
+    default: false
+  },
+  endpoint: {
+    type: String,
+    default: '/tables/all'
+  }
+})
+
+const mainStore = useMainStore()
+
+const lightBorderStyle = computed(() => mainStore.lightBorderStyle)
+
+const lightBgStyle = computed(() => mainStore.lightBgStyle)
+
+const tableTrStyle = computed(() => mainStore.tableTrStyle)
+
+const tableTrOddStyle = computed(() => mainStore.tableTrOddStyle)
+
+const darkMode = computed(() => mainStore.darkMode)
+
+const items = ref([])
+
+const isModalActive = ref(false)
+
+const isModalDangerActive = ref(false)
+
+const perPage = ref(10)
+
+const currentPage = ref(0)
+
+const checkedRows = ref([])
+
+const itemsPaginated = computed(
+  () => items.value.slice(perPage.value * currentPage.value, perPage.value * (currentPage.value + 1))
+)
+
+const numPages = computed(() => Math.ceil(items.value.length / perPage.value))
+
+const currentPageHuman = computed(() => currentPage.value + 1)
+
+const pagesList = computed(() => {
+  const pagesList = []
+
+  for (let i = 0; i < numPages.value; i++) {
+    pagesList.push(i)
+  }
+
+  return pagesList
+})
+
+const remove = (arr, cb) => {
+  const newArr = []
+
+  arr.forEach(item => {
+    if (!cb(item)) {
+      newArr.push(item)
+    }
+  })
+
+  return newArr
+}
+
+const checked = (isChecked, client) => {
+  if (isChecked) {
+    checkedRows.value.push(client)
+  } else {
+    checkedRows.value = remove(checkedRows.value, row => row.id === client.id)
+  }
+}
+
+const trimTo = (length, text) => {
+  return String(text).slice(0, length) + '...'
+}
+
+onMounted(async () => {
+  const { data: { data: tables } } = await useFetch('get', props.endpoint)
+  items.value = tables
+})
+</script>
+
+<template>
+  <modal-box
+    v-model="isModalActive"
+    title="Sample modal"
+  >
+    <p>Lorem ipsum dolor sit amet <b>adipiscing elit</b></p>
+    <p>This is sample modal</p>
+  </modal-box>
+
+  <modal-box
+    v-model="isModalDangerActive"
+    button="danger"
+    button-label="Ya, hapus"
+    has-cancel
+    large-title="Apakah anda yakin ingin menghapus pelanggan ini?"
+    @confirm="() => onConfirm('delete')"
+    @cancel="userId = ''"
+  >
+    <p>Jika pelanggan ini memiliki pesanan belum terselesaikan, <b>anda tidak dapat menghapus pelanggan ini.</b></p>
+    <p>Agar lebih aman, anda hanya dapat menghapus pelanggan yang telah mengakhiri pemesanan atau selesai mereservasi meja!</p>
+    <p>Menghapus data pelanggan akan berdampak kepada pesanan, otomatis histori pesanan akan ikut terhapus.</p>
+  </modal-box>
+
+  <div
+    v-if="checkedRows.length"
+    class="bg-opacity-50 p-3 dark:bg-gray-800"
+    :class="lightBgStyle"
+  >
+    <span
+      v-for="checkedRow in checkedRows"
+      :key="checkedRow.id"
+      class="inline-block px-2 py-1 rounded-sm mr-2 text-sm dark:bg-gray-700"
+      :class="lightBgStyle"
+    >
+      {{ checkedRow.name }}
+    </span>
+  </div>
+
+  <table>
+    <thead>
+      <tr>
+        <th v-if="checkable" />
+        <th>Nomor Meja</th>
+        <th>Token Meja</th>
+        <th>Status Meja</th>
+        <th>Pelanggan Meja</th>
+        <th>Pesanan Dibuat</th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-if="itemsPaginated.length">
+        <tr
+          v-for="(table, index) in itemsPaginated"
+          :key="table.id"
+          :class="[tableTrStyle, index % 2 === 0 ? tableTrOddStyle : '']"
+        >
+          <checkbox-cell
+            v-if="checkable"
+            @checked="checked($event, table)"
+          />
+          <td data-label="TableNumber">
+            {{ table.number }}
+          </td>
+          <td data-label="TableToken">
+            {{ trimTo(20, table.token) }}
+          </td>
+          <td data-label="IsReserved">
+            {{ table.isReserved ? "Direservasi" : "Kosong" }}
+          </td>
+          <td data-label="TableCustomer">
+            {{ table.customer[0] ? table.customer[0].name : '-' }}
+          </td>
+          <td data-label="TableOrders">
+            {{ table.orders.length }} Pesanan
+          </td>
+        </tr>
+      </template>
+      <template v-else>
+        <tr>
+          <td
+            :class="[tableTrStyle, 'text-center py-4']"
+            colspan="999"
+          >
+            Belum ada data saat ini...
+          </td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+  <div
+    :class="lightBorderStyle"
+    class="p-3 lg:px-6 border-t dark:border-gray-800"
+  >
+    <level>
+      <jb-buttons>
+        <jb-button
+          v-for="page in pagesList"
+          :key="page"
+          :active="page === currentPage"
+          :label="page + 1"
+          :outline="darkMode"
+          small
+          @click="currentPage = page"
+        />
+      </jb-buttons>
+      <small>Page {{ currentPageHuman }} of {{ numPages ? numPages : 1 }}</small>
+    </level>
+  </div>
+</template>
--- src/views/DaftarMeja.vue
+++ src/views/DaftarMeja.vue
@@ -1,11 +1,26 @@
 <script setup>
 import { ref } from 'vue'
 
+import { useFetch } from '@/composition/useFetch'
+import { usePrint } from '@/composition/usePrint'
+import CardComponent from '@/components/CardComponent.vue'
 import HeroBar from '@/components/HeroBar.vue'
+import JbButton from '@/components/JbButton.vue'
+import ListOfTable from '@/components/Tables/ListOfTable.vue'
 import MainSection from '@/components/MainSection.vue'
+import Notification from '@/components/Notification.vue'
 import TitleBar from '@/components/TitleBar.vue'
 
 const titleStack = ref(['Beranda', 'Daftar Meja'])
+
+const showNotification = ref(false)
+const notificationColor = ref('')
+const notificationMessage = ref('')
+
+const handleOnPrint = async () => {
+  const { data } = await useFetch('get', '/tables/all')
+  usePrint(data.data, 'daftar-meja')
+}
 </script>
 
 <template>
@@ -14,6 +29,28 @@ const titleStack = ref(['Beranda', 'Daftar Meja'])
   <hero-bar>Daftar Meja</hero-bar>
 
   <main-section>
-    Daftar Meja
+    <notification
+      v-if="showNotification"
+      :color="notificationColor"
+      :icon="mdiTableOff"
+    >
+      <b>{{ notificationMessage }}</b>
+    </notification>
+
+    <card-component
+      title="Daftar Meja"
+      has-table
+      :header-icon="false"
+    >
+      <template #header-tool>
+        <jb-button
+          label="Cetak Data"
+          small
+          @click="handleOnPrint"
+        />
+      </template>
+
+      <ListOfTable />
+    </card-component>
   </main-section>
 </template>
