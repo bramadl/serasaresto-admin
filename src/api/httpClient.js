4cfa08afa4835b95285e82be216d9ae797154943
bramadl <thenamesbram@gmail.com>
2022-06-22 19:10:40 +0700

    fix: use axios interceptor

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
 
 export const httpClient = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('auth._token.local')}`
-  }
+  baseURL: 'http://localhost:8000/api'
+})
+
+httpClient.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem('auth._token.local')}`
+  return config
+}, (error) => {
+  return Promise.reject(error)
 })
