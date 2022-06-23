d68be803a020585906ef72306e193f2876e84fc3
bramadl <thenamesbram@gmail.com>
2022-06-23 19:12:56 +0700

    feat: daftar menu

--- src/views/DaftarMenu.vue
+++ src/views/DaftarMenu.vue
@@ -1,11 +1,21 @@
 <script setup>
 import { ref } from 'vue'
 
+import { useFetch } from '@/composition/useFetch'
+import { usePrint } from '@/composition/usePrint'
+import CardComponent from '@/components/CardComponent.vue'
 import HeroBar from '@/components/HeroBar.vue'
+import JbButton from '@/components/JbButton.vue'
+import ListOfMenus from '@/components/Tables/ListOfMenus.vue'
 import MainSection from '@/components/MainSection.vue'
 import TitleBar from '@/components/TitleBar.vue'
 
 const titleStack = ref(['Beranda', 'Daftar Menu'])
+
+const handleOnPrint = async () => {
+  const { data } = await useFetch('get', '/menus/all')
+  usePrint(data.data, 'daftar-menu')
+}
 </script>
 
 <template>
@@ -14,6 +24,20 @@ const titleStack = ref(['Beranda', 'Daftar Menu'])
   <hero-bar>Daftar Menu</hero-bar>
 
   <main-section>
-    Daftar Menu
+    <card-component
+      title="Daftar Menu"
+      has-table
+      :header-icon="false"
+    >
+      <template #header-tool>
+        <jb-button
+          label="Cetak Data"
+          small
+          @click="handleOnPrint"
+        />
+      </template>
+
+      <ListOfMenus />
+    </card-component>
   </main-section>
 </template>
--- src/views/DaftarPemesanan.vue
+++ src/views/DaftarPemesanan.vue
@@ -1,11 +1,21 @@
 <script setup>
 import { ref } from 'vue'
 
+import { useFetch } from '@/composition/useFetch'
+import { usePrint } from '@/composition/usePrint'
+import CardComponent from '@/components/CardComponent.vue'
 import HeroBar from '@/components/HeroBar.vue'
+import JbButton from '@/components/JbButton.vue'
+import ListOfTable from '@/components/Tables/ListOfTable.vue'
 import MainSection from '@/components/MainSection.vue'
 import TitleBar from '@/components/TitleBar.vue'
 
 const titleStack = ref(['Beranda', 'Daftar Pemesanan'])
+
+const handleOnPrint = async () => {
+  const { data } = await useFetch('get', '/tables/all')
+  usePrint(data.data, 'daftar-meja')
+}
 </script>
 
 <template>
@@ -14,6 +24,20 @@ const titleStack = ref(['Beranda', 'Daftar Pemesanan'])
   <hero-bar>Daftar Pemesanan</hero-bar>
 
   <main-section>
-    Daftar Pemesanan
+    <card-component
+      title="Daftar Pemesanan"
+      has-table
+      :header-icon="false"
+    >
+      <template #header-tool>
+        <jb-button
+          label="Cetak Data"
+          small
+          @click="handleOnPrint"
+        />
+      </template>
+
+      <ListOfTable />
+    </card-component>
   </main-section>
 </template>
